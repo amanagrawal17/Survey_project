50ba691751bae4b2339044d6f41d76af6c3ccab7
Aman <amanagrawal1782@gmail.com>
2024-04-03 11:07:35 +0530

    ghvghvbhj

--- surveyapp/tests.py
+++ surveyapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+# from .models import User
 
-# Create your tests here.
+# print("this is test")
+# class UserModelTest(TestCase):
+#     def test_create_user(self):
+#         print ("inside test create fun")
+#         email = "test@example.com"
+#         # name = "test user"
+#         password = "textpassword"
+        
+#         user = User.objects.create_user(email=email, name=name, password=password)
+#         print("Userr :", user)
+#         self.assertEqual(user.email, email)
+#         # self.assertEqual(user.name, name)
+#         self.assertFalse(user.is_active)
+#         self.assertFalse(user.is_admin)
+#         self.assertFalse(user.is_staff)
+#         self.assertTrue(user.check_password(password))
